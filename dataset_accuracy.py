a3cc32f23160829104cc67a8efe1f615fc6cea89
AhmadAmr <ahmedamr18841@gmail.com>
2021-06-04 05:51:47 +0200

    dataset

--- dataset_accuracy.py
+++ dataset_accuracy.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from RDC import RDC_classfication
+print('ddddd')
 data = pd.read_csv('sample_out.csv')
 print(RDC_classfication(data))
 
